2c5d20882967e184e58fed46a0de193afcbccfa9
Adi Saripuloh <adisaripuloh@gmail.com>
2017-07-15 17:40:19 +0700

    delete images konfirmasi and update sql

--- atol.sql
+++ atol.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `atol` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `atol`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
 --
 -- Host: localhost    Database: atol
@@ -14,29 +16,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `kota`
---
-
-DROP TABLE IF EXISTS `kota`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `kota` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nama_kota` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `kota`
---
-
-LOCK TABLES `kota` WRITE;
-/*!40000 ALTER TABLE `kota` DISABLE KEYS */;
-/*!40000 ALTER TABLE `kota` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `pengguna`
@@ -81,11 +60,12 @@ CREATE TABLE `pesanan` (
   `jam_berangkat` int(11) NOT NULL,
   `no_kursi` int(11) NOT NULL,
   `nama_pemesan` varchar(45) NOT NULL,
-  `kontak` varchar(12) NOT NULL,
-  `pembayaran` tinyint(4) NOT NULL,
+  `kontak` varchar(50) NOT NULL,
+  `pembayaran` int(11) NOT NULL,
   `foto_konfirmasi` varchar(45) DEFAULT NULL,
+  `key_konfirmasi` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,7 +74,6 @@ CREATE TABLE `pesanan` (
 
 LOCK TABLES `pesanan` WRITE;
 /*!40000 ALTER TABLE `pesanan` DISABLE KEYS */;
-INSERT INTO `pesanan` VALUES (1,'Bandung','Jatinangor','2017-07-13',10,1,'Adi Saripuloh','080',0,NULL),(2,'Bandung','Jatinangor','2017-07-13',10,1,'Adi Saripuloh','080',0,NULL),(3,'Jatinangor','Bandung','2017-07-13',12,9,'Adi Saripuloh','080',0,NULL),(4,'Jatinangor','Bandung','2017-07-13',12,9,'Adi Saripuloh','080',0,NULL),(5,'Bandung','Jatinangor','2017-07-28',11,10,'Adi Saripuloh','080',0,NULL),(6,'Bandung','Jatinangor','2017-07-21',11,12,'Adi Saripuloh','080',0,NULL),(7,'Bandung','Jatinangor','2017-07-06',9,11,'Adi Saripuloh','080',0,NULL),(8,'Bandung','Jatinangor','2017-07-13',10,9,'Adi Saripuloh','080',0,NULL),(9,'Bandung','Jatinangor','2017-07-12',1,12,'','',0,NULL),(10,'Bandung','Jatinangor','2017-07-12',1,8,'','',0,NULL),(11,'Bandung','Jatinangor','2017-07-12',7,8,'','',0,NULL),(12,'Bandung','Jatinangor','2017-07-26',1,1,'Adi Saripuloh','080',0,NULL),(13,'Bandung','Jatinangor','2017-07-26',1,4,'Adi Saripuloh','08000000000',0,NULL),(14,'Bandung','Jatinangor','2017-07-12',1,1,'','',0,NULL);
 /*!40000 ALTER TABLE `pesanan` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -107,4 +86,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-12 14:15:02
+-- Dump completed on 2017-07-15 17:37:46
